42ce542f02017ee7148101889df5a6b9be1e39ac
Matheus Matsukawa <matheusmatsu@gmail.com>
2022-01-13 15:37:41 -0300

    add initial filesystem emulator closure

---
+++ src/utils/files.ts
@@ -0,0 +1,31 @@
+import { User } from '../dtos' 
+
+export const files = (req: string, user: User) => {
+  const db = {
+    users: [
+        {name: "felipe", password: "felipe123"},
+        {name: "matheus", password: "matheus123"},
+        {name: "otavio", password: "otavio123"},
+        {name: "rafael", password: "rafael123"}
+    ]
+  }
+
+  if(req === "GET-USER") {
+    return (() => {
+      const userFromDb = db.users.filter((elem) => elem.name === user.name)[0]
+      
+      if(!userFromDb) throw "User not found"
+      
+      return userFromDb
+    })
+  }
+  else if(req === "AUTH-USER") {
+    return (() => {
+      const userFromDb = db.users.filter((elem) => elem.name === user.name)[0]
+      if(!userFromDb) throw "User not found"
+      if(userFromDb.password !== user.password) throw "Wrong password"
+
+      return userFromDb
+    })
+  }
+}
