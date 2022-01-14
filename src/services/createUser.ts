a7d6c17cb4fcf2fc910ea3b131494e6f2a717041
Matheus Matsukawa <matheusmatsu@gmail.com>
2022-01-14 13:24:31 -0300

    fix put request to wasabi

--- src/services/createUser.ts
+++ src/services/createUser.ts
@@ -1,13 +1,14 @@
-import { kvSimulationGet } from "../utils/kvSimulationGet";
-import { kvSimulationPut } from "../utils/kvSimulationPut";
+import { kvSimulationGet } from "../utils/kvSimulationGet"
+import { kvSimulationPut } from "../utils/kvSimulationPut"
 
 export async function createUser(event: FetchEvent) {
-    try {
-        const user = await event.request.json();
-        const db = await kvSimulationGet();
-        const put = await kvSimulationPut(db.users.concat(user));
-        return { body: put, status: 200 };
-    } catch(err) {
-        return { body:"Expected payload " + err, status: 404 };
-    }
+  try {
+    const user = await event.request.json()
+    const db = await kvSimulationGet()
+    db.users.push(user)
+    const put = await kvSimulationPut(db)
+    return { body: put, status: 200 }
+  } catch (err) {
+    return { body: "Expected payload " + err, status: 404 }
+  }
 }
