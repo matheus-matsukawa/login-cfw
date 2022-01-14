b39b7b2af6b773cf1ea4254139a9555719b757de
Matheus Matsukawa <matheusmatsu@gmail.com>
2022-01-14 16:38:17 -0300

    add next steps

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,3 @@
-import { testeRoute } from "./testeRoute"
 import { createTokenRoute } from "./createTokenRoute"
 import { loginRoute } from "./loginRoute"
 import { createUserRoute } from "./createUserRoute"
@@ -7,7 +6,6 @@ import { updateUserRoute } from "./updateUserRoute"
 import { deleteUserRoute } from "./deleteUserRoute"
 
 export const routes: any = {
-  "/rotaDeTeste": testeRoute,
   "/createtoken": createTokenRoute,
   "/login": loginRoute,
   "/create": createUserRoute,
@@ -15,3 +13,27 @@ export const routes: any = {
   "/update": updateUserRoute,
   "/delete": deleteUserRoute
 }
+
+/* 
+USERS
+create user: POST para /users (JÁ EXISTE/ATUALIZAR) - Felipe
+  Verificar dados obrigatórios
+  Verificar validade dos dados(email único)
+
+read user: GET para /users?id=IDUSUARIO (JÁ EXISTE) Otávio
+
+update user: PATCH para /users?id=IDUSUARIO (JÁ EXISTE/ATUALIZAR) Otávio
+  Id não pode ser alterado
+
+destroy user: DELETE para /users?id=IDUSUARIO (JÁ EXISTE/ATUALIZAR)
+
+TO-DOS
+create todo: POST para /todos?id=IDUSUARIO(NOVO) - Matheus
+  verificar o que é obrigatório
+  
+read todo: GET para /todos?id=IDUSUARIO&todoid=TODOID(NOVO) - Matheus
+
+update todo: PATCH para /todos?id=IDUSUARIO&todoid=TODOID(NOVO) Felipe
+
+destroy todo: DELETE para /todos?id=IDUSUARIO&todoid=TODOID(NOVO) Otávio
+*/
