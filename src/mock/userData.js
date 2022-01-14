6c7cb8eb2a31379d1b7717d06dc6d5d470ba44c7
Matheus Matsukawa <matheusmatsu@gmail.com>
2022-01-14 15:39:15 -0300

    update mock

--- src/mock/userData.js
+++ src/mock/userData.js
@@ -1,28 +1,79 @@
 module.exports = {
   users: [
     {
+      id: "455900d3-8cba-42a0-bdd5-d8db82f775c9",
       name: "felipe",
       password: "felipe123",
       email: "felipe@gmail.com",
       about:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nisi leo, ultricies quis diam suscipit, maximus rutrum est. Aenean aliquam rhoncus elit ac volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque imperdiet augue a lectus accumsan imperdiet. Sed dictum sit amet lorem in iaculis. Vivamus porttitor ipsum eu auctor tristique. Nam accumsan, neque ac pulvinar maximus, mi purus lobortis turpis, quis porta eros mauris vitae odio. Integer porttitor lectus ut egestas pellentesque. In scelerisque mi elementum ante viverra, sit amet egestas neque sollicitudin. Maecenas semper sapien commodo, accumsan erat in, euismod libero. Cras malesuada id nisi ac feugiat.",
-      idade: 24
+      idade: 24,
+      todos: [
+        {
+          id: "cefb416d-f634-42d8-a243-20786a33239f",
+          date: new Date(),
+          name: "Maecenas nunc tellus.",
+          descrição:
+            "Donec consequat tellus elit. Nunc faucibus est neque, a mattis lorem rhoncus et. Aenean sagittis blandit elit vel ultricies. Etiam lacinia urna vulputate viverra lacinia. Vivamus convallis ut quam eu auctor. Praesent et vehicula eros. Cras luctus porta viverra."
+        },
+        {
+          id: "b1ad122a-f28d-4a79-8cb7-36c374434e67",
+          date: new Date(),
+          name: "Nec dictum augue ultricies quis.",
+          descrição:
+            "Donec consequat tellus elit. Nunc faucibus est neque, a mattis lorem rhoncus et. Aenean sagittis blandit elit vel ultricies. Etiam lacinia urna vulputate viverra lacinia. Vivamus convallis ut quam eu auctor. Praesent et vehicula eros. Cras luctus porta viverra."
+        }
+      ]
     },
     {
+      id: "d5a7582a-42de-4e59-a68c-180d19a85077",
       name: "matheus",
       password: "matheus123",
       email: "matheus@gmail.com",
       about:
         "Morbi mollis sit amet elit in efficitur. Ut non ex semper, venenatis quam sit amet, facilisis ex. Praesent lorem augue, aliquam nec tincidunt id, malesuada ut augue. Fusce ultrices rutrum diam at consectetur. Cras rhoncus augue eu metus sollicitudin eleifend. Suspendisse tincidunt ex consequat nunc feugiat, ac aliquet tellus efficitur. Vivamus facilisis porta massa. Maecenas sit amet lectus sit amet mauris lacinia dapibus nec in sem. Vivamus tincidunt erat sit amet faucibus pulvinar. Integer condimentum erat eu felis fringilla, sit amet suscipit nibh gravida. Donec lacinia felis a diam posuere luctus. Suspendisse iaculis mi vitae lacinia hendrerit. Nam id lacus eget eros vehicula vehicula vitae et nunc.",
-      idade: 26
+      idade: 26,
+      todos: [
+        {
+          id: "c624149b-ed87-4358-b4de-d26d40c9c747",
+          date: new Date(),
+          name: "Nullam risus est.",
+          descrição:
+            "Donec consequat tellus elit. Nunc faucibus est neque, a mattis lorem rhoncus et. Aenean sagittis blandit elit vel ultricies. Etiam lacinia urna vulputate viverra lacinia. Vivamus convallis ut quam eu auctor. Praesent et vehicula eros. Cras luctus porta viverra."
+        },
+        {
+          id: "0190a026-24a1-4728-ae7a-90dee778ee92",
+          date: new Date(),
+          name: "Iaculis in fringilla vel.",
+          descrição:
+            "Donec consequat tellus elit. Nunc faucibus est neque, a mattis lorem rhoncus et. Aenean sagittis blandit elit vel ultricies. Etiam lacinia urna vulputate viverra lacinia. Vivamus convallis ut quam eu auctor. Praesent et vehicula eros. Cras luctus porta viverra."
+        }
+      ]
     },
     {
+      id: "f15a89d8-6cd0-4ea0-a029-cf5f3158ea71",
       name: "otavio",
       password: "otavio123",
       email: "otavio@gmail.com",
       about:
         "Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus eu arcu dolor. Donec eget eros in purus aliquam maximus vitae eget tellus. Donec ut leo vel metus semper consequat eu quis augue. Aenean mollis hendrerit consequat. Sed odio urna, eleifend vitae vulputate non, consequat non eros. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam placerat vitae tellus sed dapibus. Suspendisse potenti. Pellentesque sagittis lectus sit amet augue molestie, dignissim sodales felis tempus. Donec non placerat purus. Pellentesque risus nisi, vehicula ac massa eget, tincidunt vestibulum neque.",
-      idade: 21
+      idade: 21,
+      todos: [
+        {
+          id: "9530c218-e62b-4c97-899f-f20c449101cf",
+          date: new Date(),
+          name: "Nam et metus erat.",
+          descrição:
+            "Donec consequat tellus elit. Nunc faucibus est neque, a mattis lorem rhoncus et. Aenean sagittis blandit elit vel ultricies. Etiam lacinia urna vulputate viverra lacinia. Vivamus convallis ut quam eu auctor. Praesent et vehicula eros. Cras luctus porta viverra."
+        },
+        {
+          id: "c01f5004-78f5-48b8-8317-12435729a64c",
+          date: new Date(),
+          name: "Sed vitae eros in augue euismod sodales.",
+          descrição:
+            "Donec consequat tellus elit. Nunc faucibus est neque, a mattis lorem rhoncus et. Aenean sagittis blandit elit vel ultricies. Etiam lacinia urna vulputate viverra lacinia. Vivamus convallis ut quam eu auctor. Praesent et vehicula eros. Cras luctus porta viverra."
+        }
+      ]
     }
   ]
 }
