307b8946e3786de61cd1f79c77a377a3fb24146f
Matheus Matsukawa <matheusmatsu@gmail.com>
2022-01-14 13:24:51 -0300

    add seed database helpers

---
+++ src/mock/data.js
@@ -0,0 +1,8 @@
+module.exports = {
+  users: [
+    { name: "felipe", password: "felipe123" },
+    { name: "matheus", password: "matheus123" },
+    { name: "otavio", password: "otavio123" },
+    { name: "rafael", password: "rafael123" }
+  ]
+}
---
+++ src/mock/seed.js
@@ -0,0 +1,15 @@
+const fetch = require("node-fetch")
+const db = require("./data")
+
+async function seed(body) {
+  await fetch(
+    `https://s3.wasabisys.com/wasabi.e-inscricao.tech/ei/estagiarios/mock.json`,
+    {
+      method: "PUT",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(body)
+    }
+  )
+}
+
+seed(db)
