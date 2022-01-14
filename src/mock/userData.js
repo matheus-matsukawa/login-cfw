e3faf879ac49e58f0b3cc2b85d1896b7a596ed0b
Matheus Matsukawa <matheusmatsu@gmail.com>
2022-01-14 15:55:08 -0300

    update mock

--- src/mock/userData.js
+++ src/mock/userData.js
@@ -8,7 +8,7 @@ module.exports = {
       about:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nisi leo, ultricies quis diam suscipit, maximus rutrum est. Aenean aliquam rhoncus elit ac volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque imperdiet augue a lectus accumsan imperdiet. Sed dictum sit amet lorem in iaculis. Vivamus porttitor ipsum eu auctor tristique. Nam accumsan, neque ac pulvinar maximus, mi purus lobortis turpis, quis porta eros mauris vitae odio. Integer porttitor lectus ut egestas pellentesque. In scelerisque mi elementum ante viverra, sit amet egestas neque sollicitudin. Maecenas semper sapien commodo, accumsan erat in, euismod libero. Cras malesuada id nisi ac feugiat.",
       age: 24,
-      todos: [
+      "to-dos": [
         {
           id: "cefb416d-f634-42d8-a243-20786a33239f",
           date: new Date(),
@@ -33,7 +33,7 @@ module.exports = {
       about:
         "Morbi mollis sit amet elit in efficitur. Ut non ex semper, venenatis quam sit amet, facilisis ex. Praesent lorem augue, aliquam nec tincidunt id, malesuada ut augue. Fusce ultrices rutrum diam at consectetur. Cras rhoncus augue eu metus sollicitudin eleifend. Suspendisse tincidunt ex consequat nunc feugiat, ac aliquet tellus efficitur. Vivamus facilisis porta massa. Maecenas sit amet lectus sit amet mauris lacinia dapibus nec in sem. Vivamus tincidunt erat sit amet faucibus pulvinar. Integer condimentum erat eu felis fringilla, sit amet suscipit nibh gravida. Donec lacinia felis a diam posuere luctus. Suspendisse iaculis mi vitae lacinia hendrerit. Nam id lacus eget eros vehicula vehicula vitae et nunc.",
       age: 26,
-      todos: [
+      "to-dos": [
         {
           id: "c624149b-ed87-4358-b4de-d26d40c9c747",
           date: new Date(),
@@ -58,7 +58,7 @@ module.exports = {
       about:
         "Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus eu arcu dolor. Donec eget eros in purus aliquam maximus vitae eget tellus. Donec ut leo vel metus semper consequat eu quis augue. Aenean mollis hendrerit consequat. Sed odio urna, eleifend vitae vulputate non, consequat non eros. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam placerat vitae tellus sed dapibus. Suspendisse potenti. Pellentesque sagittis lectus sit amet augue molestie, dignissim sodales felis tempus. Donec non placerat purus. Pellentesque risus nisi, vehicula ac massa eget, tincidunt vestibulum neque.",
       age: 21,
-      todos: [
+      "to-dos": [
         {
           id: "9530c218-e62b-4c97-899f-f20c449101cf",
           date: new Date(),
