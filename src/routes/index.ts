fb721027b16e8a8ea106aac55a131a0e23d4bdc1
Otávio Jr. O. Nunes <OtavioJr08@gmail.com>
2022-01-14 09:24:30 -0300

    correção mesclagem

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,9 @@
-import { testeRoute } from "./testeRoute";
-<<<<<<< HEAD
-import { loginRoute } from "./loginRoute";
+import { testeRoute } from "./testeRoute"
+import { createTokenRoute } from "./createTokenRoute"
+import { loginRoute } from "./loginRoute"
 
 export const routes: any = {
   "/rotaDeTeste": testeRoute,
-  '/login': loginRoute
-=======
-import {createTokenRoute} from "./createTokenRoute";
-
-export const routes: any = {
-  "/rotaDeTeste": testeRoute,
-  "/createtoken": createTokenRoute
->>>>>>> c40cd114d0db81d48dcdb02f07b6cd43735351a0
+  "/createtoken": createTokenRoute,
+  "/login": loginRoute
 }
