f22de608243e3eb7a018fd115a2dbd13e8c282fb
felipelp121 <felopesserra@id.uff.br>
2022-01-16 17:13:00 -0300

    adicionando tempo de expiração no token

--- src/utils/createToken.ts
+++ src/utils/createToken.ts
@@ -1,4 +1,4 @@
 import {sign} from "jsonwebtoken";
 export async function createToken(payload: object) { 
-    return sign(payload, "secret", {algorithm: "HS256"});  
+    return sign(payload, "secret", {algorithm: "HS256", expiresIn: "15d"});  
 }
